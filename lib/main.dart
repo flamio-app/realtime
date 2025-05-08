d19191e748f8dc64192ab948ebdf4a2e3fcd192f
flamio-app <flamio.official@gmail.com>
2025-05-08 21:33:42 +0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kayıt',
+      title: 'Davut',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const RegisterScreen(),
     );
